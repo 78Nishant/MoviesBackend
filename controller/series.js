fc6f1db635879f1a8ec8879a51eeefa435c08a48
Nishant Kumar <145116546+78Nishant@users.noreply.github.com>
2024-07-06 09:54:33 +0530

    Update series.js

--- controller/series.js
+++ controller/series.js
@@ -5,3 +5,8 @@ exports.getProduct=async(req,res)=>{
     const serie=await Serie.find();
     res.status(200).json(serie)
 }
+exports.rankProduct=async(req,res)=>{
+    const id=req.params.id;
+    const movie=await Serie.find({rank : id});
+    res.status(200).json(movie);
+}
